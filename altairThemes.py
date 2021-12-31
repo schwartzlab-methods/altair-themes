191a38b5a2693051a3df01fec9292be05f7f27cd
Gregory W. Schwartz <ifthenelse2244@gmail.com>
2021-12-31 12:32:14 -0500

    Initial.

---
+++ altairThemes.py
@@ -0,0 +1,82 @@
+# Altair themes
+# By
+# Gregory W. Schwartz
+
+'''
+Use:
+
+sys.path.append("/path/to/parent/directory/of/altairThemes.py")
+
+if True:  # In order to bypass isort when saving
+    import altairThemes
+
+# register the custom theme under a chosen name
+alt.themes.register("publishTheme", altairThemes.publishTheme)
+
+# enable the newly registered theme
+alt.themes.enable("publishTheme")
+'''
+
+import altair as alt
+
+
+def publishTheme():
+    # Typography
+    font = "Arial"
+    labelFont = "Arial"
+    sourceFont = "Arial"
+    fontSize = 10.66670036  # Size 8pt for publishing
+
+    # Axes
+    axisColor = "#000000"
+
+    return {
+        "config": {
+            "view": {
+                "height": 80,  # 80 for publishing
+                "width": 100,  # 100 for publishing
+                "strokeOpacity": 0,  # Despine
+                "strokeWidth": 0.756,
+            },
+            "style": {
+                "guide-title": {
+                    "fontSize": fontSize
+                },
+                "guide-label": {
+                    "fontSize": fontSize
+                }
+            },
+            "title": {
+                "fontSize": fontSize,
+                "font": font,
+                "fontColor": "#000000",
+                "fontWeight": "normal",
+            },
+            "axis": {
+                "domainColor": "#000000",
+                "domainWidth": 0.756,
+                "grid": False,
+                "gridWidth": 0.756,
+                "labelAngle": 0,
+                "labelFont": labelFont,
+                "labelFontSize": fontSize,
+                "tickColor": axisColor,
+                "tickWidth": 0.756,
+                "titleFont": font,
+                "titleFontSize": fontSize,
+                "titleFontWeight": "normal",
+            },
+            # For individual axis
+            # "axisX": {
+            #     "grid": False,
+            #     "domainColor": "#000000",
+            #     "labelFont": labelFont,
+            #     "labelFontSize": fontSize,
+            #     "labelAngle": 0,
+            #     "tickColor": axisColor,
+            #     "titleFont": font,
+            #     "titleFontSize": fontSize,
+            #     "titleFontWeight": "normal",
+            # },
+        }
+    }
